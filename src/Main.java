c161b662212c9c16ff290ce3f96df6f31d3fd8b2
Ana Medina <112995260+AnaGabyy@users.noreply.github.com>
2022-11-24 13:03:30 -0300

    Creación de objetos en la Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Está funcionando");
+
+        Shape s1 = new Circle("red", false, 5.0);
+
+        Square s2 = new Square(); //cargar usando setters
+        s2.setColor("Rosado");
+        s2.setFilled(false);
+
+        Shape s3 = new Rectangle("red", false, 1.0, 2.0);
+
+        Shape s4 = new Square(6.6);
+
+
+
     }
 }
